ff777197a85b76663ef4c20e807cb6b40110f5db
Revanth Ch <revanthcse1@gmail.com>
2026-02-08 03:50:00 -0500

    finish problem 1 2 3

--- src/03-grade-calculator.js
+++ src/03-grade-calculator.js
@@ -26,4 +26,29 @@
  */
 export function calculateGrade(score, hasExtraCredit) {
   // Your code here
+  if (score < 0 || score > 100) {
+    return "INVALID";
+  }
+
+  if (hasExtraCredit) {
+    score = score + 5;
+
+    if (score > 100) {
+      score = 100;
+    } else {
+      score = score;
+    }
+  }
+
+  if (score >= 90 && score <= 100) {
+    return "A";
+  } else if (score >= 80) {
+    return "B";
+  } else if (score >= 70) {
+    return "C";
+  } else if (score >= 60) {
+    return "D";
+  } else {
+    return "F";
+  }
 }
