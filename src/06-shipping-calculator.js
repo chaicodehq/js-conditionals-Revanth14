ad0c90b29e7f8639f0d774c89edfc43dd9eb97f2
Revanth Ch <revanthcse1@gmail.com>
2026-02-08 06:45:06 -0500

    problem 6

--- src/06-shipping-calculator.js
+++ src/06-shipping-calculator.js
@@ -29,5 +29,29 @@
  * @returns {number} Shipping cost, 0 for free shipping, or -1 for invalid input
  */
 export function calculateShipping(weight, country, orderTotal) {
-  // Your code here
+  if (weight <= 0 || orderTotal < 0) {
+    return -1;
+  }
+  if (country === "US") {
+    if (weight <= 1 && orderTotal <= 50) {
+      return 5;
+    } else if (weight <= 5 && orderTotal <= 50) {
+      return 10;
+    } else if (weight > 5 && orderTotal <= 50) {
+      return 15;
+    } else if (orderTotal > 50) {
+      return 0;
+    }
+  }
+  if (country !== "US") {
+    if (weight < 1 && orderTotal <= 100) {
+      return 15;
+    } else if (weight <= 5 && orderTotal <= 100) {
+      return 25;
+    } else if (weight > 5 && orderTotal <= 100) {
+      return 40;
+    } else if (orderTotal > 100) {
+      return 0;
+    }
+  }
 }
